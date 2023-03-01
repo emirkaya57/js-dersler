7f1a9a75832c040d9448309e81030facfe791ae4
Emirhan Kaya <emirhankaya356@gmail.com>
2023-03-01 14:35:54 +0300

    try-catch

---
+++ try-catch.js
@@ -0,0 +1,10 @@
+
+
+/* let num = prompt('sayi girin');
+try {
+    if (num == '') throw 'Boş olamaz'
+    if (isNaN(num)) throw 'sayı değil';
+    num = Number(num);
+} catch (error) {
+    alert(error)
+} */
